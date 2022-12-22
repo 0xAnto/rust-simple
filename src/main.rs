ac8a8a2899fe09d543b6bfd4f022a2d5f28025f1
0xAnto <antonyranjith17@gmail.com>
2022-12-23 01:01:10 +0530

    for loop

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-mod strings;
 fn main() {
-strings::string_operation();
+    // Create an array of integers from 0 to 9
+    let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    println!("Array length {}", numbers.len());
+    // Loop through the array
+    for number in numbers.iter() {
+        // Print each number
+        println!("{}", number);
+    }
 }
